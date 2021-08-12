e59914f3a11b14c4d4381869fabf5115af165a0e
Yang <82341009+yangzhengca@users.noreply.github.com>
2021-08-12 13:34:07 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ import thunk from "redux-thunk";
 import reducers from "./reducers";
 import "./index.css";
 
-// const store= createStore(reducers,compose(applyMiddleware(thunk)))
-const store = createStore(
-  reducers,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+const store= createStore(reducers,compose(applyMiddleware(thunk)))
+// const store = createStore(
+//   reducers,
+//   compose(
+//     applyMiddleware(thunk),
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//   )
+// );
 
 ReactDOM.render(
   <Provider store={store}>
